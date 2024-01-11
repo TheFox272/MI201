c2b2bb2d21358841d80b27ac4d34b0887906b67d
TheFox272 <fabien.girard27@gmail.com>
2024-01-11 09:46:33 +0100

    test

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # MI201
-Adversiala attack of neural networks
+Adversial attack of neural networks
+
+```math
+\frac{\delta loss(f(x, \omega), y(x))}{\delta x}
+
+||x_{origin} - x_{noised}||_{\infty} \leq \epsilon = 4 / 255
+
+z = net(x)
+\delata = request_grad()
+z_n = net(x + \delta)
+obj = ||z - z_n||
+```
---
+++ origin.py
@@ -0,0 +1,64 @@
+import os
+import matplotlib.pyplot as plt
+import torch, torchvision
+
+os.chdir("./coco")
+
+im1 = torchvision.io.read_image("217730183_8f58409e7c_z.jpg").float() / 255
+im2 = torchvision.io.read_image("541870527_8fe599ec04_z.jpg").float() / 255
+im3 = torchvision.io.read_image("2124681469_7ee4868747_z.jpg", mode=torchvision.io.ImageReadMode.RGB).float() / 255
+im4 = torchvision.io.read_image("2711568708_89f2308b85_z.jpg").float() / 255
+im5 = torchvision.io.read_image("2928196999_acd5471d23_z.jpg").float() / 255
+im6 = torchvision.io.read_image("3016145160_497da1b387_z.jpg").float() / 255
+im7 = torchvision.io.read_image("4683642953_2eeda0820e_z.jpg").float() / 255
+im8 = torchvision.io.read_image("6911037487_cc68a9d5a4_z.jpg").float() / 255
+im9 = torchvision.io.read_image("8139728801_60c233660e_z.jpg").float() / 255
+
+im1 = torch.nn.functional.interpolate(im1.unsqueeze(0), size=520)[0]
+im2 = torch.nn.functional.interpolate(im2.unsqueeze(0), size=520)[0]
+im3 = torch.nn.functional.interpolate(im3.unsqueeze(0), size=520)[0]
+im4 = torch.nn.functional.interpolate(im4.unsqueeze(0), size=520)[0]
+im5 = torch.nn.functional.interpolate(im5.unsqueeze(0), size=520)[0]
+im6 = torch.nn.functional.interpolate(im6.unsqueeze(0), size=520)[0]
+im7 = torch.nn.functional.interpolate(im7.unsqueeze(0), size=520)[0]
+im8 = torch.nn.functional.interpolate(im8.unsqueeze(0), size=520)[0]
+im9 = torch.nn.functional.interpolate(im9.unsqueeze(0), size=520)[0]
+
+visu = torch.cat([im1, im2, im3, im4, im5, im6, im7, im8, im9], dim=-1)
+visu = visu.cpu().numpy().transpose(1, 2, 0)
+dpi = plt.rcParams['figure.dpi']
+width_px = 1600
+height_px = 200
+plt.figure(figsize=(width_px / dpi, height_px / dpi))
+plt.imshow(visu)
+plt.axis('off')
+plt.show()
+
+W = torchvision.models.segmentation.DeepLabV3_ResNet50_Weights.COCO_WITH_VOC_LABELS_V1
+net = torchvision.models.segmentation.deeplabv3_resnet50(weights=W)
+net = net.cuda()
+
+with torch.no_grad():
+  x = torch.stack([im1,im2,im3,im4,im5,im6,im7,im8,im9],dim=0)
+  x = (W.transforms())(x).cuda()
+  z = net(x)["out"] # on prédit des cartes de score de confiance
+  z = z[:,[0,8,12,15],:,:] # we keep only person, cat and dog class
+  _,z = z.max(1) # on prend le meilleur score
+
+# visualisation des prédictions : il faut transformer les indices de classes en couleur
+couleur = torch.zeros(9,3,520,520).cuda()
+couleur[:,0,:,:] = (z==1).float() # red for cat
+couleur[:,1,:,:] = (z==2).float() # green for dog
+couleur[:,2,:,:] = (z==3).float() # blue for person
+
+visu = torch.cat([im1,im2,im3,im4,im5,im6,im7,im8,im9],dim=-1)
+visubis = torch.cat([couleur[i] for i in range(9)],dim=-1).cpu()
+visu = torch.cat([visu,visubis],dim=1)
+visu = visu.cpu().numpy().transpose(1,2,0)
+dpi = plt.rcParams['figure.dpi']
+width_px = 1600
+height_px = 400
+plt.figure(figsize=(width_px/dpi, height_px/dpi))
+plt.imshow(visu)
+plt.axis('off')
+plt.show()
